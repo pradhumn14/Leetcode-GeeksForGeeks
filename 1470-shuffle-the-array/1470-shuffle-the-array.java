7434606f62c287176c7d7e96e2dd44427f2340c1
Pradhumn Agrawal <pradhumnagrawal14@gmail.com>
2023-01-25 14:15:58 +0530

    Time: 1 ms (39.29%), Space: 45.7 MB (27.71%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        
+        int arr[] = new int[nums.length];
+        int c=0;
+        for (int i = 0; i < nums.length-1; i = i + 2) {
+            arr[i] = nums[c];
+            arr[i + 1] = nums[c + n];
+            c++;
+        }
+        
+        return arr;
+        
+    }
+}
