0d58dc0c12190386a76a576df8afa3c5458b4fb8
Pradhumn Agrawal <pradhumnagrawal14@gmail.com>
2023-01-27 16:07:07 +0530

    Time: 39 ms (53.61%), Space: 71.4 MB (14.27%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,20 +1,38 @@
 class Solution {
-    public  List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res  = new HashSet<>();
-        if(nums.length==0) return new ArrayList<>(res);
+    public List<List<Integer>> threeSum(int[] nums) {
+        
         Arrays.sort(nums);
-        for(int i=0; i<nums.length-2;i++){
-            int j =i+1;
-           int  k = nums.length-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum==0)res.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
-                else if (sum >0) k--;
-                else if (sum<0) j++;
+        List<List<Integer>> ans = new LinkedList<>();
+        
+        for (int i = 0; i < nums.length -2; i++) {
+            if (i == 0 || i > 0 && nums[i] != nums[i-1]) {
+                int low = i +1;
+                int high = nums.length -1;
+                int sum = 0 - nums[i];
+                
+                while (low < high) {
+                    if (nums[low] + nums[high] == sum) {
+                        ans.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        
+                        while (low < high && nums[low] == nums[low+1]) {
+                            low++;
+                        }
+                        while (low < high && nums[high] == nums[high - 1]) {
+                            high--;
+                        }
+                        low++;
+                        high--;
+                    } else if (nums[low] + nums[high] < sum) {
+                        low++;
+                    } else {
+                        high--;
+                    }
+                }
+                
             }
-
+            
         }
-        return new ArrayList<>(res);
-
+        return ans;
+    
     }
 }
