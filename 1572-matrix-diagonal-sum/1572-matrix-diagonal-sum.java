eede81db4c0c71c72e56e823fb707c884a860249
Pradhumn Agrawal <pradhumnagrawal14@gmail.com>
2023-05-08 09:59:46 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (45.49%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,10 +1,11 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
+        
         int i = 0;
         int j = 0;
         int sum = 0;
         
-        while(i < mat.length && j < mat.length) {
+        while (i < mat.length && j < mat.length) {
             sum += mat[i][j];
             i++;
             j++;
@@ -13,13 +14,13 @@ class Solution {
         i = 0;
         j = mat.length - 1;
         
-        while(i < mat.length && j >= 0) {
+        while (i < mat.length && j >= 0) {
             sum += mat[i][j];
             i++;
             j--;
         }
         
-        if(mat.length % 2 != 0) {
+        if (mat.length % 2 != 0) {
             sum -= mat[mat.length / 2][mat.length / 2];
         }
         
